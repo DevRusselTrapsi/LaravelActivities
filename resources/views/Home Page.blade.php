0138dd769c2a1fbe6ca3d514d48dd5084bcfa478
Trapsi, Markr Russel B. BSIT - 3B <78476407+DevRusselTrapsi@users.noreply.github.com>
2022-03-29 16:31:05 +0800

    Midterm Exam

---
+++ resources/views/Home Page.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('title','Home Page')
+
+<div class="container">
+    <div class="row">
+        <div>
+            <h1>Home Page Content</h1>
+        </div>
+    </div>
+
+</div>
---
+++ resources/views/Post Page.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('title','Post Page')
+
+<div class="container">
+    <div class="row">
+        <div>
+            <h1>Post Page Content</h1>
+        </div>
+    </div>
+
+</div>
